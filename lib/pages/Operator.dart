b28eb9a56a90ab905fe47741234b49cacbeb8ea8
PrachiAlbiorix <prachik.albiorix@gmail.com>
2025-01-22 16:09:36 +0530

    task_operator

--- lib/pages/Operator.dart
+++ lib/pages/Operator.dart
@@ -6,7 +6,7 @@ class OperatorsPage extends StatelessWidget {
     int a = 2;
     int b = 3;
 
-    //Arithmetic opertion
+    // Arithmetic operations
     var sum = a + b;
     var difference = a - b;
     var negation = -difference;
@@ -15,18 +15,18 @@ class OperatorsPage extends StatelessWidget {
     var quotient = b ~/ a;
     var remainder = b % a;
 
-    String ArithmeticResult = '''
-    Arithmetic Operations:   
+    String arithmeticResult = '''
+Arithmetic:
 
-    value: a=$a, b=$b 
-    Sum: $sum
-    Difference: $difference
-    Negation: $negation
-    Product: $product
-    Division: $division
-    Quotient: $quotient
-    Remainder: $remainder
-    ''';
+a= $a, b= $b
+Sum: $sum
+Difference: $difference
+Negation: $negation
+Product: $product
+Division: $division
+Quotient: $quotient
+Remainder: $remainder
+''';
 
     // Relational Operations
     var c = a > b;
@@ -36,63 +36,114 @@ class OperatorsPage extends StatelessWidget {
     var g = b == a;
     var h = b != a;
 
-    String RelationalResult = '''
-    Relational Operations:    
+    String relationalResult = '''
+Relational:
 
-    value: a=$a, b=$b 
-    a>b: $c
-    a<b: $d
-    a>=b: $e
-    a<=b: $f
-    b==a: $g
-    b!=a: $h
-    ''';
+a= $a, b= $b
+a>b: $c
+a<b: $d
+a>=b: $e
+a<=b: $f
+b==a: $g
+b!=a: $h
+''';
 
-    //type test operations
+    // Type test operations
+    // ignore: unnecessary_type_check
     var isInteger = a is int;
     var isNotDouble = b is! double;
 
     String typeTestResult = '''
-    Type Test Operations:    
+Type Test:
 
-    value: a=$a, b=$b 
-    a is int: $isInteger
-    b is not double: $isNotDouble
-    ''';
+a= $a, b= $b
+a is int: $isInteger
+b is not double: $isNotDouble
+''';
 
-    //Bitwise Operators
+    // Bitwise Operators
     var C = a & b;
     var D = a | b;
     var E = a ^ b;
     var F = ~a;
     var G = a << b;
     var H = a >> b;
-    var I = -a >>> b;
 
-    String BitwiseResult = '''
-    Bitwise Operations: 
+    String bitwiseResult = '''
+Bitwise:
 
-    a & b: $C
-    a | b: $D
-    a ^ b: $E
-    ~a: $F
-    a << b: $G
-    a >> b: $H
-    -a >>> b: $I
-    ''';
+a= $a, b= $b
+a & b: $C
+a | b: $D
+a ^ b: $E
+~a: $F
+a << b: $G
+a >> b: $H
+''';
 
-    //logical operator
+    // Logical operators
     bool P = a > 10 && b < 10;
     bool Q = a > 10 || b < 10;
     bool R = !(a > 10);
 
-    String LogicalResult = '''
-    Logical Operations:   
+    String logicalResult = '''
+Logical:
 
-    Using AND: $P
-    using OR: $Q
-    using NOT: $R
-    ''';
+a= $a, b= $b
+Using AND: $P
+Using OR: $Q
+Using NOT: $R
+''';
+
+    // Assignment Operators
+    var x = a;
+    x += b;
+    var y = a;
+    y -= b;
+    var z = a;
+    z *= b;
+    var w = b;
+    w ~/= a;
+    var v = b;
+    v %= a;
+
+    String assignmentResult = '''
+Assignment:
+
+a=$a, b=$b
+x += b: $x
+y -= b: $y
+z *= b: $z
+w ~/= a: $w
+v %= a: $v
+''';
+
+//Conditional Operator
+    var X = 30;
+    var output =
+        X > 42 ? "value greater than 10" : "value lesser than equal to 30";
+
+    String conditionalResult = '''
+Conditional:
+
+value: $X
+$output
+''';
+
+// Cascade Operators
+    var K = <int>[1, 2, 3];
+
+    K
+      ..add(4)
+      ..removeAt(0)
+      ..insert(0, 0);
+
+    String cascadeResult = '''
+Cascade:
+
+Initial list: [1, 2, 3]
+$K
+''';
 
     return Scaffold(
       appBar: AppBar(
@@ -102,62 +153,58 @@ class OperatorsPage extends StatelessWidget {
         padding: EdgeInsets.all(16),
         child: Column(
           children: [
-            Card(
-              color: Colors.blue.shade50,
-              margin: EdgeInsets.symmetric(vertical: 8),
-              child: Padding(
-                padding: EdgeInsets.all(16),
-                child: Text(
-                  ArithmeticResult,
-                  style: TextStyle(fontSize: 16, fontFamily: 'monospace'),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      _buildCard(Colors.blue.shade100, arithmeticResult),
+                      SizedBox(height: 16),
+                      _buildCard(Colors.green.shade100, assignmentResult),
+                      SizedBox(height: 16),
+                      _buildCard(Colors.pink.shade100, typeTestResult),
+                      SizedBox(height: 16),
+                      _buildCard(Colors.orange.shade100, conditionalResult),
+                    ],
+                  ),
                 ),
-              ),
-            ),
-            Card(
-              color: Colors.green.shade50,
-              margin: EdgeInsets.symmetric(vertical: 8),
-              child: Padding(
-                padding: EdgeInsets.all(16),
-                child: Text(
-                  RelationalResult,
-                  style: TextStyle(fontSize: 16, fontFamily: 'monospace'),
+                SizedBox(width: 16),
+                Expanded(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      _buildCard(Colors.yellow.shade100, relationalResult),
+                      SizedBox(height: 16),
+                      _buildCard(Colors.red.shade100, bitwiseResult),
+                      SizedBox(height: 16),
+                      _buildCard(Colors.purple.shade100, logicalResult),
+                      SizedBox(height: 16),
+                      _buildCard(Colors.lightBlue.shade100, cascadeResult),
+                    ],
+                  ),
                 ),
-              ),
-            ),
-            Card(
-              color: Colors.pink.shade50,
-              margin: EdgeInsets.symmetric(vertical: 8),
-              child: Padding(
-                padding: EdgeInsets.all(16),
-                child: Text(
-                  typeTestResult,
-                  style: TextStyle(fontSize: 16, fontFamily: 'monospace'),
-                ),
-              ),
-            ),
-            Card(
-              color: Colors.yellow.shade50,
-              margin: EdgeInsets.symmetric(vertical: 8),
-              child: Padding(
-                padding: EdgeInsets.all(16),
-                child: Text(
-                  LogicalResult,
-                  style: TextStyle(fontSize: 16, fontFamily: 'monospace'),
-                ),
-              ),
-            ),
-            Card(
-              color: Colors.red.shade50,
-              margin: EdgeInsets.symmetric(vertical: 8),
-              child: Padding(
-                padding: EdgeInsets.all(16),
-                child: Text(
-                  BitwiseResult,
-                  style: TextStyle(fontSize: 16, fontFamily: 'monospace'),
-                ),
-              ),
+              ],
             ),
           ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildCard(Color color, String content) {
+    return Card(
+      color: color,
+      margin: EdgeInsets.zero,
+      child: Padding(
+        padding: EdgeInsets.all(16),
+        child: Align(
+          alignment: Alignment.centerLeft,
+          child: Text(
+            content,
+            style: TextStyle(fontSize: 16, fontFamily: 'monospace'),
+          ),
         ),
       ),
     );
